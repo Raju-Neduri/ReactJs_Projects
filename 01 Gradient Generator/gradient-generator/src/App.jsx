e9e1d65aab77ef7a3108551f14f3643a0d927093
Neduri Raju <rajneduri123@gmail.com>
2025-10-31 19:05:51 +0530

    add project and h1 tag for gradient generator

---
+++ 01 Gradient Generator/gradient-generator/src/App.jsx
@@ -0,0 +1,15 @@
+function App() {
+  return (
+    <>
+      <div className="min-h-screen">
+        <div className="w-9/12 mx-auto">
+          <div>
+            <h1 className="text-3xl font-bold">🎨 Gradient Generator</h1>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default App;
