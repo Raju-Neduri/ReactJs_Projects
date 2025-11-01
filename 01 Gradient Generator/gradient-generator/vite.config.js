b8433c3b72b6eeab3a4e6dcb7a8375108e89e347
Neduri Raju <rajneduri123@gmail.com>
2025-11-01 22:46:31 +0530

    Pushed

--- 01 Gradient Generator/gradient-generator/vite.config.js
+++ 01 Gradient Generator/gradient-generator/vite.config.js
@@ -5,5 +5,5 @@ import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: "/ReactJs_Projects/01_Gradient_Generator/gradient-generator",
+  base: "/ReactJs_Projects/01_Gradient_Generator/gradient-generator/",
 });
