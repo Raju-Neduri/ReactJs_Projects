1848ab6320e449d25dd25c6d46c0fc1c794600ab
Neduri Raju <rajneduri123@gmail.com>
2025-11-01 18:33:38 +0530

    Add usestate to dynamically update the num and type from user and print on screen

--- 01 Gradient Generator/gradient-generator/src/App.jsx
+++ 01 Gradient Generator/gradient-generator/src/App.jsx
@@ -1,10 +1,32 @@
+import { useState } from "react";
+
 function App() {
+  const [num, setNum] = useState(12);
+  const [type, setType] = useState("linear");
+
   return (
     <>
-      <div className="min-h-screen">
+      <div className="min-h-screen bg-white py-12">
         <div className="w-9/12 mx-auto">
-          <div>
-            <h1 className="text-3xl font-bold">🎨 Gradient Generator</h1>
+          <div className="flex justify-between">
+            <h1 className="text-3xl font-bold">
+              🎨 Gradient Generator {num}, {type}
+            </h1>
+            <div className="flex gap-4">
+              <input
+                type="text"
+                className="border border-slate-300 bg-white rounded-lg w-[100px] p-2"
+                placeholder="12"
+                onChange={(e) => setNum(Number(e.target.value))}
+              />
+              <select
+                className="border border-slate-300 bg-white rounded-lg w-[100px] p-2"
+                onChange={(e) => setType(e.target.value)}
+              >
+                <option value="linear">Linear</option>
+                <option value="Radial">Radial</option>
+              </select>
+            </div>
           </div>
         </div>
       </div>
