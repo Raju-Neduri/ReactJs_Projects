742075c6fdcf3d56cb9621221d5dae7ad3e33d76
Neduri Raju <rajneduri123@gmail.com>
2025-11-04 22:33:50 +0530

    options add

--- 02 avatar-generator/src/App.jsx
+++ 02 avatar-generator/src/App.jsx
@@ -1,6 +1,44 @@
 import "remixicon/fonts/remixicon.css";
 import "animate.css";
 
+const data = [
+  {
+    label: "Illustration",
+    value: "illustration",
+    url: "https://api.dicebear.com/7.x/avataaars/svg?seed=",
+  },
+  {
+    label: "Cartoon",
+    value: "cartoon",
+    url: "https://api.dicebear.com/7.x/adventurer/svg?seed=",
+  },
+  {
+    label: "Sketchy",
+    value: "sketchy",
+    url: "https://api.dicebear.com/7.x/croodles/svg?seed=",
+  },
+  {
+    label: "Robots",
+    value: "robots",
+    url: "https://api.dicebear.com/7.x/bottts/svg?seed=",
+  },
+  {
+    label: "Art",
+    value: "art",
+    url: "https://api.dicebear.com/7.x/pixel-art/svg?seed=",
+  },
+  {
+    label: "Male",
+    value: "male",
+    url: "https://randomuser.me/api/portraits/men/",
+  },
+  {
+    label: "Female",
+    value: "female",
+    url: "https://randomuser.me/api/portraits/women/",
+  },
+];
+
 function App() {
   return (
     <>
@@ -25,7 +63,11 @@ function App() {
 
           <div className="w-full space-y-4">
             <select className="bg-slate-900/60 w-full p-3 rounded-xl">
-              <option value="male">male</option>
+              {data.map((item, index) => (
+                <option value={item.value} key={index}>
+                  {item.label}
+                </option>
+              ))}
             </select>
 
             <div className="bg-slate-900/60 w-full p-4 rounded-xl text-left">
