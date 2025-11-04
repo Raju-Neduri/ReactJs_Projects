edf01e44e68c4d639a5889dab3651d0b12a236ec
Neduri Raju <rajneduri123@gmail.com>
2025-11-04 20:15:10 +0530

    chore: initialize React project for Avatar Generator

---
+++ 02 avatar-generator/src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <>
+      <div className="min-h-screen bg-gradient-to-br from-slate-900 via-black to-slate-900 flex items-center justify-center text-white">
+        <div className="cla">Raj</div>
+      </div>
+    </>
+  );
+}
+
+export default App;
