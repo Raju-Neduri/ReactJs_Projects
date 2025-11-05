ff2339b7d7af8ea20917e89f91c4d16b8e089075
Neduri Raju <rajneduri123@gmail.com>
2025-11-05 10:14:55 +0530

    updated

--- 02-avatar-generator/vite.config.js
+++ 02-avatar-generator/vite.config.js
@@ -5,5 +5,5 @@ import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: "/ReactJs_Projects/02-avatar-generator",
+  base: "/ReactJs_Projects/02-avatar-generator/",
 });
